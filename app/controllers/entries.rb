f8212c5f333c7068bc891ae633e10e7c4720e4d1
Grant Willey <email": null>
2016-06-07 16:20:11 -0500

    fixed null entries

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,3 +1,18 @@
 get '/users/:user_id/entries/new' do
+  @user = current_user
    erb :"entries/new"
 end
+
+post '/users/:user_id/entries' do
+  @entry = Entry.new(params[:entry])
+  @user = User.new(params[:user])
+
+  @entry.user_id = current_user.id
+  if @user && @entry.save
+    redirect '/'
+  else
+    @error = "LEAVE NOTHING BLANK"
+    erb :"entries/new"
+  end
+end
+
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
+  validates :title, presence: true
+  validates :body, presence: true
   # Remember to create a migration!
 end
